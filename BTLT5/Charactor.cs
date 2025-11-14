d5eeefa9d609e33ac0e7847aea98528ec9ed4939
Nha <Nhavotan2k6@gmail.com>
2025-11-14 22:25:04 +0700

    player

---
+++ BTLT5/Charactor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTLT5
+{
+    public class Charactor
+    {
+        public Bitmap sprite;
+        public int Column, Row;
+        public bool leftPressed = false;
+        public bool rightPressed = false;
+        public bool upPressed = false;
+        public bool downPressed = false;
+        public int x = 10, y = 50;
+        public bool move;
+        public int index;
+        public Charactor()
+        {
+            sprite = new Bitmap("Sasuke.png");
+            index = 0;
+            move = true;
+        }
+        public void KeyUp(Keys key)
+        {
+            leftPressed = false;
+            rightPressed = false;
+            upPressed = false;
+            downPressed = false;
+        }
+        public void KeyDow(Keys key)
+        {
+            if (key == Keys.Left)
+            {
+                leftPressed = true;
+                Row = 1;
+            }
+            if (key == Keys.Right)
+            {
+                rightPressed = true;
+                Row = 2;
+            }
+            if (key == Keys.Up)
+            {
+                upPressed = true;
+                Row = 3;
+            }
+            if (key == Keys.Down)
+            {
+                downPressed = true;
+                Row = 0;
+            }
+        }
+        public void Draw(Graphics g)
+        {
+            move = leftPressed || rightPressed || upPressed || downPressed;
+            if (move)
+            {
+                Column = index % 4;
+                g.DrawImage(sprite, x, y, new Rectangle(Column * 48, Row * 64, 48, 64), GraphicsUnit.Pixel);
+                index++;
+                if (index >= 4)
+                {
+                    index = 1;
+                }
+            }
+            else
+            {
+                index = 0;
+                g.DrawImage(sprite, x, y, new Rectangle(0, Row * 64, 48, 64), GraphicsUnit.Pixel);
+            }
+        }
+        public void Update()
+        {
+            if (leftPressed) x -= 5;
+            if (rightPressed) x += 5;
+            if (upPressed) y -= 5;
+            if (downPressed) y += 5;
+        }
+    }
+}
---
+++ BTLT5/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTLT5
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap sprite;
+        public Bitmap backBuffer;
+        public Graphics g;
+        public Charactor player;
+        public Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            player = new Charactor();
+            backBuffer= new Bitmap(ClientSize.Width, ClientSize.Height);
+            g = this.CreateGraphics();
+            timer = new Timer();
+            timer.Interval = 100;
+            timer.Enabled = true;
+            timer.Tick += timer_Tick;
+            Render();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            Render();
+            player.KeyUp(e.KeyCode);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            player.KeyDow(e.KeyCode);
+        }
+
+        
+
+        public void Render()
+        {
+            Graphics g1= Graphics.FromImage(backBuffer);
+            g1.Clear(Color.White);
+            player.Draw(g1);
+            g.DrawImageUnscaled(backBuffer, 0, 0);
+            g1.Dispose();
+
+
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            player.Update();
+            Render();
+            
+        }
+    }
+    
+}
